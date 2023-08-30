fe1c1b3fe1e271a8ca9d348149f0e13cdf9d62d4
Clara-Padilha <143611697+Clara-Padilha@users.noreply.github.com>
2023-08-30 12:30:50 +0000

    alteração de cor

--- MeuApp/App.js
+++ MeuApp/App.js
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00ff00',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
